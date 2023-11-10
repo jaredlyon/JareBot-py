0004224109107cdd9f67962f56657b6167623f8a
Jared Lyon <jaredalyon@gmail.com>
2023-11-10 02:10:38 -0500

    restart

--- bot.py
+++ bot.py
@@ -1,59 +1,20 @@
 import discord
-from discord.ext import commands
-import json
-import logging
-import os
 
-# sends all bot logs back to the console
-handler = logging.StreamHandler()
-handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-logging.getLogger().setLevel(logging.INFO)
-logging.getLogger().addHandler(handler)
-
-# sets up the bot
 intents = discord.Intents.default()
-intents.members = True
-bot = commands.Bot(command_prefix='$', intents=intents)
+intents.message_content = True
 
-# set up commands
-# set presence to "with Python3.9"
-# register all cogs
-# logs loaded cogs to the console
-@bot.event
+client = discord.Client(intents=intents)
+
+@client.event
 async def on_ready():
-    print(f'{bot.user} is connected to the following guilds:')
-    for guild in bot.guilds:
-        print(f'{guild.name}(id: {guild.id})')
-    await bot.change_presence(activity=discord.Game(name='with Python3.9'))
-    for filename in os.listdir('./cogs'):
-        if filename.endswith('.py'):
-            await bot.load_extension(f'cogs.{filename[:-3]}')
-            print(f'Loaded {filename[:-3]}')
+    print(f'We have logged in as {client.user}')
 
-# logs the use of commands in the console
-@bot.event
-async def on_command(ctx):
-    print(f'{ctx.author} used {ctx.command}')
-
-# set up commands
-@bot.event
+@client.event
 async def on_message(message):
-    # don't respond to ourselves
-    if message.author == bot.user:
+    if message.author == client.user:
         return
 
-# reloads all the cogs
-@bot.command()
-async def reload(ctx):
-    for filename in os.listdir('./cogs'):
-        if filename.endswith('.py'):
-            bot.reload_extension(f'cogs.{filename[:-3]}')
-            print(f'Reloaded {filename[:-3]}')
-    await ctx.send('Reloaded all cogs')
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
 
-# read the config file
-with open('config.json') as f:
-    config = json.load(f)
-
-# then runs the bot using the token from the config file
-bot.run(config['token'], log_handler = handler)
+client.run('MzMwNDg2NDg0Mjg3MzU2OTI4.GB1Rbb.n8ZV98pB_m8g1ud7i5K5oTjGVXJ25X6pg1PWm8')
